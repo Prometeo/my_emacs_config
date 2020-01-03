8decd6958b26ba17cefa7a3b8dcf9a82fdcf4a90
Prometeo <xairi.valdivia@gmail.com>
2020-01-02 22:17:28 -0500

    Added company quickhelp

--- myinit.el
+++ myinit.el
@@ -52,16 +52,19 @@
 
 ;; [[file:~/.emacs.d/myinit.org::*Company][Company:1]]
 (use-package company
-:ensure t
-:config
-(setq company-idle-delay 0)
-(setq company-minimum-prefix-length 3)
-(setq company-show-numbers t)
-(setq company-tooltip-limit 10)
-(setq company-tooltip-align-annotations t)
-(setq company-tooltip-flip-when-above t)
-(global-company-mode t)
-)
+    :ensure t
+    :config
+        (setq company-idle-delay 0)
+        (setq company-minimum-prefix-length 0)
+        (setq company-show-numbers t)
+        (setq company-tooltip-limit 10)
+        (setq company-tooltip-align-annotations t)
+        (setq company-tooltip-flip-when-above t)
+        (global-company-mode t))
+(use-package company-quickhelp
+    :ensure t)
+(company-quickhelp-mode 1)
+(setq company-quickhelp-delay 0)
 
 (use-package company-irony
 :ensure t
@@ -706,6 +709,8 @@ narrowed."
     :ensure t)
 (add-hook 'python-mode-hook 'anaconda-mode)
 (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
+(eval-after-load "company"
+ '(add-to-list 'company-backends '(company-anaconda :with company-capf)))
 
 (defun python--encoding-comment-required-p ()
   (re-search-forward "[^\0-\177]" nil t))
