e0b734f37bcde64bc7955cfcc678561887314802
Prometeo <xairi.valdivia@gmail.com>
2020-01-04 12:41:30 -0500

    Reconfigured treemacs

--- myinit.el
+++ myinit.el
@@ -927,13 +927,13 @@ narrowed."
           treemacs-collapse-dirs              3
           treemacs-silent-refresh             nil
           treemacs-change-root-without-asking nil
-          treemacs-sorting                    'alphabetic-desc
+          treemacs-sorting                    'alphabetic-case-insensitive-asc
           treemacs-show-hidden-files          t
           treemacs-never-persist              nil
           treemacs-is-never-other-window      nil
           treemacs-goto-tag-strategy          'refetch-index)
-
-    (treemacs-follow-mode t)
+     ;; do not follow the file
+    (treemacs-follow-mode -1)
     (treemacs-filewatch-mode t))
   :bind
   (:map global-map
